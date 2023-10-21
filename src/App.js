bc289a6f9c050cd0b17fbfbcb229a29cf58a2a40
swap665 <sk0021033@gmail.com>
2023-10-21 23:08:00 +0530

    swapnil

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn Reactok 
         </a>
       </header>
     </div>
